e9786d423e4df77f50b2c7644bd92fda36d68853
Alejandro Osorio <avot3110@hotmail.com>
2023-05-09 12:15:10 -0400

    Fetch Lab

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+    fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+  };
   
-}
+  fetchBooks();
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
